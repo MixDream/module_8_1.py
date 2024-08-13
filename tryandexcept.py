0ec505c919a310b110b3c0f934dee903790591ec
MixDream <max_juldashev@mail.ru>
2024-08-13 20:07:26 +0300

    Update tryandexcept.py

--- tryandexcept.py
+++ tryandexcept.py
@@ -1,8 +1,8 @@
-def add_everything_up(a: int | float | str, b: int | float | str) -> int | float | str:
+def add_everything_up(a, b):
     try:
         return a + b
     except TypeError:
-        return str(a) + str(b)
+        return f"{a}{b}"
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
